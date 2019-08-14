329cc5e34a76442fc2d9d7a1c5fcadcc9326b330
Ruan Beukes <ruan.coder@gmail.com>
2019-08-14 15:19:55 +0800

    First cut. Still verymuch hardcoded values. WIP - add CLI args.

---
+++ RecursiveRename/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace RecursiveRename
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var renameFolderPath = Path.Combine("c:", "repos", "healthtracker-app", "src");
+            Console.WriteLine(renameFolderPath);
+            RenameFolder(renameFolderPath);
+        }
+
+        private static void RenameFolder(string renameFolderPath)
+        {
+            if (Directory.GetDirectories(renameFolderPath).Any())
+                foreach (var folder in Directory.GetDirectories(renameFolderPath))
+                {
+                    if (Directory.GetDirectories(folder).Any())
+                        RenameFolder(folder);
+                    else
+                    {
+                        RenameFiles(folder);
+                    }
+                }
+            else
+            {
+                RenameFiles(renameFolderPath);
+            }
+        }
+
+        private static void RenameFiles(string folder)
+        {
+            foreach (var file in Directory.GetFiles(folder, "*.spec.ts"))
+            {
+                var newFileName = file.Split(".spec.ts")[0] + ".spec.old.ts";
+                File.Move(file, newFileName);
+                Console.WriteLine($"Renamed {file} => {newFileName}");
+            }
+        }
+    }
+}
